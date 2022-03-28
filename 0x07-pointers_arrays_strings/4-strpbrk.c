b40901580d513c9af3a89c73fd75f151d3957bcc
evelynampadu <evelyn.ampadu@gmail.com>
2022-03-28 05:18:12 -0700

    set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,8 +6,7 @@
 *   * @s: string to search
 *    * @accept: stringcontaining the bytes to look for
 *     *
-*      * Return: pointer to the byte in s that matches one of the bytes in accept 
-*           * or NULL if no such byte is found
+*      * Return:pointer to the byte in s that matches one of the bytes in accept* or NULL if no such byte is found
 */
 
 char *_strpbrk(char *s, char *accept)
@@ -25,5 +24,6 @@ char *_strpbrk(char *s, char *accept)
 		}
 		s++;
 	}
+
 	return (NULL);
 }
