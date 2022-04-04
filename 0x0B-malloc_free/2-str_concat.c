724ee02f5e1eb85f8312a87035b41a5255289ed3
evelynamapdu <evelyn.ampadu@gmail.com>
2022-04-04 06:17:12 -0700

    grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,7 @@
 *  * str_concat - Entry point
 *   *@s1: string 1
 *    *@s2: string 2
-*     * Return: pointer should point to a newly allocated space in memory or NULL
+*     * Return: pointer shd point to a newly allocated space in memory or NULL
 */
 
 char *str_concat(char *s1, char *s2)
